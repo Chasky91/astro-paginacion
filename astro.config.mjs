6013f4b5fb83f91906e4264ebbf97449fe558d6a
Chasky91 <david_c91@outlook.com>
2026-09-10 21:40:15 -0300

    caMBIOS XD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField} from 'astro/config';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
+  env: {
+    schema:{
+        API_URL:envField.string({ context: "server", access: "public" })
+    }
+  }
+});
---
+++ src/lib/usuarios.mjs
@@ -0,0 +1,11 @@
+import { API_URL } from "astro:env/server"
+
+export async function getUsuarios() {
+  const res = await fetch(`${API_URL}/productos`, {
+    
+    signal: AbortSignal.timeout(8000),
+  })
+
+  if (!res.ok) throw new Error(`API respondió ${res.status}`)
+  return res.json()
+}
