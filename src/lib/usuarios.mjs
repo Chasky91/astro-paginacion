5d8c847f30ff478129b6a0e0c3ab5bea08f2a9c2
Chasky91 <david_c91@outlook.com>
2026-09-24 20:26:21 -0300

    con  navars

--- src/lib/usuarios.mjs
+++ src/lib/usuarios.mjs
@@ -1,9 +1,8 @@
 import { API_URL } from "astro:env/server"
 
-export async function getUsuarios() {
-  const res = await fetch(`${API_URL}/user/users`, {
+export async function getUsuarios(page, limit) {
+  const res = await fetch(`${API_URL}/user/users?page=${page}&limit=${limit}`, {
     
-    signal: AbortSignal.timeout(8000),
   })
   if (!res.ok) throw new Error(`API respondió ${res.status}`)
   return res.json()
